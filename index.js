22e5182d3e578230074884c76d2173eadcb018df
Muhammad Usama Imam <usamaimam1@outlook.com>
2020-05-14 01:03:58 +0500

    Project Init

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'Client/build')))
+app.use(require("body-parser").text());
+app.use(require('body-parser').urlencoded({ extended: false }));
+app.use(require('body-parser').json());
+// require('./Server/Services/FirebaseAdminService')
+
+// require('./Server/Routes/getUserData')(app)
+// require('./Server/Routes/getProjectsData')(app)
+app.get('/favicon.ico', (req, res) => {
+    res.sendFile(path.join(__dirname, '/Client/public/favicon.ico'))
+})
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/Client/build/index.html'))
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Mixing it up on port ${PORT}`)
+})
