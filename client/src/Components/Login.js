4a11a8dc42e155b1d2bbe696802bfd1d1cc29170
Muhammad Usama Imam <usamaimam1@outlook.com>
2020-05-15 00:48:36 +0500

    Login and Dashboard Added

---
+++ client/src/Components/Login.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import TopLogo from '../assets/Logo.gif'
+import logo from '../assets/logo-dark.png'
+class Login extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    componentDidMount() {
+
+    }
+    render() {
+        return (
+            <body class="bg-default">
+                {/* Nav Bar */}
+                <nav id="navbar-main" class="navbar navbar-horizontal navbar-transparent navbar-main navbar-expand-lg navbar-light">
+                    <div class="container">
+                        <a class="navbar-brand" href="dashboard.html">
+                            <img src={TopLogo} />
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="navbar-collapse navbar-custom-collapse collapse" id="navbar-collapse">
+                            <div class="navbar-collapse-header">
+                                <div class="row">
+                                    <div class="col-6 collapse-brand">
+                                        <a href="dashboard.html">
+                                            <img src="../assets/img/brand/blue.png" />
+                                        </a>
+                                    </div>
+                                    <div class="col-6 collapse-close">
+                                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+                                            <span></span>
+                                            <span></span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr class="d-lg-none" />
+                        </div>
+                    </div>
+                </nav>
+
+                <div class="main-content">
+                    {/* Upper div */}
+                    <div class="header bg-gradient-primary py-7 py-lg-1 pt-lg-7">
+                        <div class="container">
+                            <div class="header-body text-center mb-7">
+                                <div class="row justify-content-center">
+                                    <div class="col-xl-5 col-lg-6 col-md-8 px-5">
+                                        <h2 style={{ fontFamily: 'poppins' }} class="text-white">Welcome! CruisersADVISORS ADMIN</h2>
+                                        {/* <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for free.</p> */}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="separator separator-bottom separator-skew zindex-100">
+                            <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                                <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+                            </svg>
+                        </div>
+                    </div>
+                    {/* Sign In Div */}
+                    <div class="container mt--6 pb-5">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5 col-md-7">
+                                <div class="card bg-secondary border-0 mb-0">
+                                    <div class="card-body px-lg-5 py-lg-5">
+                                        <div class="text-center text-muted mb-4">
+                                            <img src={logo} style={{ height: 75, width: 120 }}></img>
+                                        </div>
+                                        {/* <div text-center>
+                        <text style={{ textAlign: 'center', fontSize: 12, fontFamily: 'poppins', fontWeight: 'bold', alignSelf: 'center' }}>CruisersADVISORS ADMIN</text>
+                      </div> */}
+
+                                        <form role="form">
+                                            <div class="form-group mb-3">
+                                                <div class="input-group input-group-merge input-group-alternative">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                                    </div>
+                                                    <input class="form-control" placeholder="Email" type="email" />
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="input-group input-group-merge input-group-alternative">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                                                    </div>
+                                                    <input class="form-control" placeholder="Password" type="password" />
+                                                </div>
+                                            </div>
+                                            <div class="custom-control custom-control-alternative custom-checkbox">
+                                                <input class="custom-control-input" id=" customCheckLogin" type="checkbox" />
+                                                <label class="custom-control-label" for=" customCheckLogin">
+                                                    <span class="text-muted">Remember me</span>
+                                                </label>
+                                            </div>
+                                            <div class="text-center">
+                                                <button type="button" class="btn btn-primary my-4">Sign in</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                                <div class="row mt-3">
+                                    <div class="col-6">
+                                        <a href="#" class="text-light"><small>Forgot password?</small></a>
+                                    </div>
+                                    <div class="col-6 text-right">
+                                        <a href="#" class="text-light"><small>Create new account</small></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <footer class="py-5" id="footer-main">
+                    <div class="container">
+                        <div class="row align-items-center justify-content-xl-between">
+                            <div class="col-xl-6">
+                                <div class="copyright text-center text-xl-left text-muted">
+                                    &copy; 2020 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+                                </div>
+                            </div>
+                            <div class="col-xl-6">
+                                <ul class="nav nav-footer justify-content-center justify-content-xl-end">
+                                    <li class="nav-item">
+                                        <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </body >
+        )
+    }
+}
+
+export default Login;
