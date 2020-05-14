21d0ee1540bc0f2bb63aa80e3934e7ebf6083bd0
Muhammad Usama Imam <usamaimam1@outlook.com>
2020-05-14 15:19:41 +0500

    Firebase Admin Credentials Added

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const app = express()
-
 app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(require("body-parser").text());
 app.use(require('body-parser').urlencoded({ extended: false }));
 app.use(require('body-parser').json());
+// Loads the FirebaseAdmin Client from the credentials
+require('./server/services/FirebaseAdminService')
+
 // require('./Server/Services/FirebaseAdminService')
 
 // require('./Server/Routes/getUserData')(app)
---
+++ server/services/FirebaseAdminService.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const admin = require('firebase-admin')
+const ADMIN_CONFIG = {
+    "type": process.env.FIREBASE_ADMIN_TYPE,
+    "project_id": process.env.FIREBASE_ADMIN_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_ADMIN_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_ADMIN_AUTH_URI,
+    "token_uri": process.env.FIREBASE_ADMIN_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_ADMIN_AUTH_PROVIDER_CERT_URL,
+    "client_x509_cert_url": process.env.FIREBASE_ADMIN_CLIENT_CERT_URL
+}
+console.log(ADMIN_CONFIG)
+admin.initializeApp({
+    credential: admin.credential.cert(ADMIN_CONFIG),
+    databaseURL: "https://cruisersadvisor.firebaseio.com"
+})
